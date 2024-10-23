b6760822ac2185d4a4d73e23bce36ae35002385c
Matthew Morales <mtthwmrls9@gmail.com>
2024-10-23 13:43:41 -0600

    Finish activity 2 code

--- src/gpio_interrupt.c
+++ src/gpio_interrupt.c
@@ -6,28 +6,47 @@
 #include <queue.h>
 #include "Lab5.h"
 
-QueueHandle_t queue;
+#define READ_TASK_PRIORITY      ( tskIDLE_PRIORITY + 1UL )
+#define READ_TASK_STACK_SIZE configMINIMAL_STACK_SIZE
 
+QueueHandle_t queue;
 int toggle = 1;
+
+typedef struct {
+    uint gpio;
+    uint32_t mask;
+} isr_params_t;
+
 void irq_callback(uint gpio, uint32_t event_mask)
 {
+    isr_params_t params;
     if (gpio != IN_PIN) return;
-    toggle = !toggle;
-    if (event_mask & GPIO_IRQ_EDGE_RISE) {
-        gpio_put(OUT_PIN, true);
-    } else if (event_mask & GPIO_IRQ_EDGE_FALL) {
-        gpio_put(OUT_PIN, false);
-    }
+
+    params.gpio = gpio;
+    params.mask = event_mask;
+
+    xQueueSendFromISR(queue, &params, 0);
 }
 
-void read_task (_unused void *params) {
+void read_task (__unused void *params) {
+    isr_params_t isr_params;
 
+    xQueueReceive(queue, &isr_params, 0);
+
+    if (isr_params.gpio == IN_PIN) {
+        toggle = !toggle;
+        if (isr_params.mask & GPIO_IRQ_EDGE_RISE) {
+            gpio_put(OUT_PIN, true);
+        } else if (isr_params.mask & GPIO_IRQ_EDGE_FALL) {
+            gpio_put(OUT_PIN, false);
+        }
+    }
 }
 
 int main(void)
 {
     TaskHandle_t task;
-    queue = xQueueCreate(10, sizeof(char));
+    queue = xQueueCreate(10, sizeof(isr_params_t));
 
     stdio_init_all();
 
@@ -40,7 +59,9 @@ int main(void)
 
     gpio_set_irq_enabled_with_callback(IN_PIN, GPIO_IRQ_EDGE_RISE | GPIO_IRQ_EDGE_FALL , true, irq_callback);
     
-    xTaskCreate(read_task);
+    xTaskCreate(read_task, "ReadTask",
+                READ_TASK_STACK_SIZE, NULL, READ_TASK_PRIORITY, NULL);
+    vTaskStartScheduler();
     
     while(1) __wfi();
     return 0;
